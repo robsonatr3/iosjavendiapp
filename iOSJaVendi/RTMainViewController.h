21b6e32d33b194b35a31fc1b15f937fb60bf719b
robson <robsonsky@gmail.com>
2014-09-24 22:47:46 -0300

    first commit

---
+++ iOSJaVendi/RTMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  RTMainViewController.h
+//  iOSJaVendi
+//
+//  Created by Robson Ribeiro on 9/24/14.
+//  Copyright (c) 2014 rob. All rights reserved.
+//
+
+#import "RTBaseViewController.h"
+
+@interface RTMainViewController : RTBaseViewController
+
+@end
---
+++ iOSJaVendi/RTSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  RTSearchViewController.h
+//  iOSJaVendi
+//
+//  Created by Robson Ribeiro on 9/24/14.
+//  Copyright (c) 2014 rob. All rights reserved.
+//
+
+#import "RTBaseViewController.h"
+
+@interface RTSearchViewController : RTBaseViewController
+
+@end
