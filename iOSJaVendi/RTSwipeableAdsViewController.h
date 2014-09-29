17e2cee172e00c21b097830f058bcddfb980f9b7
robson <robsonsky@gmail.com>
2014-09-29 20:56:14 -0300

    working on swipeable ads

--- iOSJaVendi/RTSwipeableAdsViewController.h
+++ iOSJaVendi/RTSwipeableAdsViewController.h
@@ -9,13 +9,20 @@
 #import "RTBaseViewController.h"
 
 @interface RTSwipeableAdsViewController : RTBaseViewController
+//<NSURLConnectionDataDelegate>
 
 @property (strong, nonatomic) NSString *stringOne;
 @property (strong, nonatomic) NSString *stringTwo;
 @property (strong, nonatomic) NSString *stringThree;
+@property (strong, nonatomic) IBOutlet UILabel *labelFour;
 
 @property (strong, nonatomic) IBOutlet UILabel *labelOne;
 @property (strong, nonatomic) IBOutlet UILabel *labelTwo;
 @property (strong, nonatomic) IBOutlet UILabel *labelThree;
+
+@property (nonatomic) long valueIndex;
+@property (strong, nonatomic) NSArray *adsArray;
+
+- (void)getJson;
 
 @end
--- iOSJaVendi/RTSwipeableAdsViewController.m
+++ iOSJaVendi/RTSwipeableAdsViewController.m
@@ -10,13 +10,24 @@
 
 @interface RTSwipeableAdsViewController ()
 
+@property (nonatomic, strong) NSMutableData *webData;
+@property (nonatomic, strong) NSURLConnection *connection;
+@property (nonatomic, strong) NSMutableArray *array;
+
 @end
 
 @implementation RTSwipeableAdsViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self getJson];
+    
+    self.valueIndex = 0;
+    
+    NSDictionary *feed = [self.adsArray objectAtIndex:self.valueIndex];
+    NSDictionary *dicOfEntry = [feed objectForKey:@"ad"];
+    int ad = [dicOfEntry[@"id"] integerValue];
 }
 
 - (void) viewWillAppear:(BOOL)animated
@@ -26,4 +37,49 @@
     self.labelThree.text = self.stringThree;
 }
 
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    [self.webData setLength:0];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    [self.webData appendData:data];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    NSLog(@"fail with error");
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    self.adsArray = [NSJSONSerialization JSONObjectWithData:self.webData options:0 error:nil];
+//    for (int i = 0; i < self.adsArray.count; i++) {
+//        NSDictionary *feed = [self.adsArray objectAtIndex:i];
+//        NSDictionary *dicOfEntry = [feed objectForKey:@"ad"];
+//        int ad = [dicOfEntry[@"id"] integerValue];
+//        NSLog(@"%i",ad);
+//    }
+   
+    
+//    for (NSDictionary *ad in arrayOfEntry) {
+//        NSDictionary *title = [ad objectForKey:@"ad"];
+//        NSString *label = [title objectForKey:@"title"];
+//        NSLog(label);
+//    }
+}
+
+- (void)getJson{
+    
+    NSURL *url = [NSURL URLWithString:@"http://192.168.1.138:3000/ads.json?category_name=Celulares"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    self.connection = [NSURLConnection connectionWithRequest:request delegate:self];
+    
+    if (self.connection) {
+        self.webData = [[NSMutableData alloc]init];
+    }
+    
+}
 @end
